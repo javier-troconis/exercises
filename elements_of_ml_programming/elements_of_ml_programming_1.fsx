bd7461d4cd17a99152f919563c35643574607a0b
javier troconis <jtroconisa@hotmail.com>
2019-10-13 18:55:37 -0500

    exercises

--- elements_of_ml_programming/elements_of_ml_programming_1.fsx
+++ elements_of_ml_programming/elements_of_ml_programming_1.fsx
@@ -1,3 +1,5 @@
+let explode (x:char seq) = Seq.foldBack (fun e s -> e::s) x []
+
 //2.1.1.a 7
 //2.1.1.c 2
 //2.1.1.e false
@@ -22,3 +24,46 @@ System.Convert.ToInt32 'Y'
 //2.2.2.c 256 is outside of the ascii range of 0-255
 //2.2.2.e ord domain is char
 //2.2.2.h ord domain is char
+//2.4.1.a 4
+//2.4.1.c [4; 5]
+//2.4.1.e "foo"
+//2.4.1.g ["c"; "o"; "b"; "o"; "l"]
+//2.4.2.a #3(3, 4, 5)
+//2.4.2.c 1
+//2.4.2.e ('a'; 'b')
+//2.4.2.g can't List.tail on empty list
+//2.4.2.i concat works on string list
+//2.4.3.a float * (string * (int list))
+//2.4.3.c (int * float) list
+//2.4.4 (1,2) is not the same type as (1,2,3). [1,2] is the same type as [1,2,3]
+//2.4.5.a
+[[[0]; []]]
+//2.4.5.c
+([""], (0, (0., "")), 0)
+//2.4.5.e
+((false, 0), '0') 
+let ``3.1.1.a`` x :float = x * x * x
+let ``3.1.1.b`` a b c = if a > b 
+                        then 
+                            if b > c 
+                            then c 
+                            else b
+                        else 
+                            if a > c
+                            then c
+                            else a
+let ``3.1.1.c`` x = List.head (List.tail (List.tail x))
+let ``3.1.1.e`` x = ``3.1.1.c`` (explode x)
+let ``3.1.1.f`` x = List.tail x @ [List.head x]
+let ``3.1.2.a`` a b c = if a > b 
+                        then 
+                            if b > c 
+                            then (a, c) 
+                            else ((if a > c then a else c), b)
+                        else 
+                            if a > c
+                            then (b, c)
+                            else ((if b > c then b else c), a)
+//3.1.3.a 8
+//3.1.3.b 11
+//3.1.3.e 18
